78e615f34e457ca0d7291aa2d72c813f99467f26
Josef Reichelt <pepareichelt@gmail.com>
2021-04-26 07:18:41 +0200

    Simple write to file solution

--- intro-to-rust/src/main.rs
+++ intro-to-rust/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello there");
+    let mut args = std::env::args().skip(1);
+    let key = args.next().unwrap();
+    let value = args.next().unwrap();
+
+    println!("Prvni argument je {} and the value is {}", key, value);
+    let contents =  format!("{}\t{}\n",key,value);
+    std::fs::write("kv.db",contents);
 }
