43275f4043a4f68ebb1932e028ab78fe570d9a95
Josef Reichelt <pepareichelt@gmail.com>
2021-07-14 13:17:36 +0200

    Finish cpp intro

---
+++ cpp-part1/1_intro.cpp
@@ -0,0 +1,15 @@
+// Intro to c++
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello There" << endl;
+
+    int num1;
+    cout << "Input a number: ";
+    cin >> num1;
+    cout << "You entered: " << num1 << endl;
+
+    return 0;
+}
