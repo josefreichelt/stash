8e9efd88064961082c95f6f7be7157d709ec2290
Josef Reichelt <pepareichelt@gmail.com>
2022-02-10 20:53:54 +0100

    Fix typo

--- nanago/main.go
+++ nanago/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Convefence"
+	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets = 50
 	fmt.Println("Welcome to", conferenceName, "booking application")
