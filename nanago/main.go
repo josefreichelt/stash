ff8946b7faac65fde666beb75d20766388ab6a54
Josef Reichelt <pepareichelt@gmail.com>
2022-02-11 19:42:39 +0100

    Data types

--- nanago/main.go
+++ nanago/main.go
@@ -3,11 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
-	
+	var remainingTickets uint8 = 50
+
+	// remainingTickets = -1
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here!")
+
+	var userName string
+	var userTickets int
+	// ask for input
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
