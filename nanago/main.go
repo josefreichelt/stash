d9f4f8edc09a0b619c0b79a2bd265d3ecb5f006c
Josef Reichelt <pepareichelt@gmail.com>
2022-02-15 16:06:27 +0100

    Ticket booking logic

--- nanago/main.go
+++ nanago/main.go
@@ -17,7 +17,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint8
 	// ask for input
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -28,5 +28,8 @@ func main() {
 	fmt.Println("Enter amount of tickets: ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
