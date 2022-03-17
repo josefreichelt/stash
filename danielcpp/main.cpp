1a9b3b59d2ea0f1d6401877ec1bd7b74a7f22ccd
Josef Reichelt <pepareichelt@gmail.com>
2022-03-17 19:59:07 +0100

    Comments

--- danielcpp/main.cpp
+++ danielcpp/main.cpp
@@ -2,6 +2,18 @@
 
 int main()
 {
+    // THIS IS A COMMENT, WOOOOOO
+
+
+    /*
+        THIS
+        IS
+        A
+        MULTI
+        LINE
+        COMMENT
+        WOOOOOO
+    */
     std::cout << "Number 1" << std::endl;
     std::cout << "Number 2" << std::endl;
     std::cout << "Number 3" << std::endl;
