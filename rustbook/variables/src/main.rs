5c2eeff2cb9cb72241020a7d8785244c2f710cfe
Josef Reichelt <pepareichelt@gmail.com>
2022-05-29 16:25:42 +0200

    Variables

---
+++ rustbook/variables/src/main.rs
@@ -0,0 +1,9 @@
+const MOJE_HUSTA_CONSTANTA: i32 = 123;
+
+fn main() {
+    let mut x = 5;
+    println!("Value of x is: {}", x);
+    x = 6;
+    println!("Value of x is: {}", x);
+    println!("Constant: {}", MOJE_HUSTA_CONSTANTA);
+}
