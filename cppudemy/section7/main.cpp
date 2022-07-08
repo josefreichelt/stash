9b40499e7521c4deef06f49ebe0db22a12c5cdae
Josef Reichelt <pepareichelt@gmail.com>
2022-07-08 10:23:31 +0200

    Assigning

--- cppudemy/section7/main.cpp
+++ cppudemy/section7/main.cpp
@@ -6,7 +6,7 @@ int main()
     // Declaring an aray
     int high_score_per_level [10] {0}; // 10 integers initialized to 0
 
-    
+    high_score_per_level[1] = 20;
 
-    std::cout << "First element" << high_score_per_level[1] << std::endl;
+    std::cout << "Second element " << high_score_per_level[1] << std::endl;
 }
