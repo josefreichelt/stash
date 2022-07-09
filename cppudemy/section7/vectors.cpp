55f17cd0fd9cc4ebe16d9c31793805fa3e672374
Josef Reichelt <pepareichelt@gmail.com>
2022-07-09 17:44:29 +0200

    Accessing vector

--- cppudemy/section7/vectors.cpp
+++ cppudemy/section7/vectors.cpp
@@ -5,7 +5,8 @@ using namespace std;
 // Vector is a dynamic array and has bounds checking
 int main()
 {
-    vector<char> vowels(5);         // Auto initialized to 0
-    vector<int> test_scores(10, 5); // Initialize 10 ints to 5
+    vector<char> vowels{'a', 'e', 'i', 'o', 'u'}; // Auto initialized to 0
+    vector<int> test_scores(10, 5);                  // Initialize 10 ints to 5
     cout << "Hello vectors" << endl;
+    cout << "Vowel: " << vowels.at(0) << endl;
 }
