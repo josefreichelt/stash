51691ccd4a9badc5af95ef43523b7add86e24b93
Josef Reichelt <pepareichelt@gmail.com>
2022-07-11 22:18:08 +0200

    Assignment operator

---
+++ cppudemy/section8/expressionandstatements.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "hello there" << endl;
+
+    // Expressions, výrazy
+    // 32 Literal
+    // favorite_number variable
+    // 1.5 + 2.8  addition
+    // 2 * 5 multiplication
+    // a > b relational
+    // a = b assignment
+
+    // Statement, příkaz, komplet linka kodu co dělá nějakou akci
+    // if a tak
+
+    int num1{10};
+    int num2{20};
+
+    num1 = num2;
+
+    cout << "num1 is " << num1 << endl;
+    cout << "num2 is " << num2 << endl;
+
+    cout << endl;
+}
